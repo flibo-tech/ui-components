b1ceab1873e6dd331293446f0e2637b8bd0d9d17
Bhaskar <bhaskar2choudhary@gmail.com>
2020-05-23 00:06:18 +0530

    added basic grid

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" class="f-container">
+    <Navigation />
     <router-view />
   </div>
 </template>
 
+<script>
+import Navigation from "./components/Navigation";
+export default {
+  components: { Navigation }
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.f-container {
+  display: grid;
+  grid-template-columns: 300px 1fr;
 }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,5 @@
+<template>
+  <nav>
+    <h1>Navigation</h1>
+  </nav>
+</template>
