03e277607d4106b2f5f3c81fc1d6ca1c6490c533
Bhaskar <bhaskar2choudhary@gmail.com>
2020-05-27 14:54:09 +0530

    updated basic navigation (functional only)

---
+++ src/components/molecular/TwoButtons.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Button />
+    <br />
+    <Button />
+  </div>
+</template>
+
+<script>
+import Button from "./../atomic/Button";
+
+export default {
+  name: "Home",
+  components: { Button }
+};
+</script>
