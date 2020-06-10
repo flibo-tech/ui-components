5110e3641237bb87523bf5340912e7a3ccb6430d
Ankit <ankit.gupta@gmail.com>
2020-06-10 16:18:50 -0400

    amzn style dictionary init

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+// Do not edit directly
+// Generated on Wed, 10 Jun 2020 20:18:14 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
