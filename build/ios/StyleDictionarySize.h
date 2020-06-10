a2eda6b205c111d7544ddcb7614086fa6d8bdc9c
Ankit <ankit.gupta@gmail.com>
2020-06-10 14:04:07 -0400

    style dictionary init

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+// Do not edit directly
+// Generated on Wed, 10 Jun 2020 18:03:49 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
