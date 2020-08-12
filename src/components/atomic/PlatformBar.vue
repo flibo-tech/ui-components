bb2ba74af8c6ffdd5ecd7fb2a608fd04a8cf7d29
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-08-12 16:26:02 +0530

    added scenario for all no of platforms, converted props into proper arrays.

--- src/components/atomic/PlatformBar.vue
+++ src/components/atomic/PlatformBar.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="platform-container" v-if="noOfPlatforms">
-      <img src="../../assets/platforms/netflix.png" v-if="noOfPlatforms === 1">
+  <div :class="platCalc" v-if="noOfPlatforms">
+    <div class="one" v-if="noOfPlatforms === 1">
+      <img src="../../assets/platforms/netflix.png" />
+    </div>
 
-     <div class="two-plat" v-if="noOfPlatforms === 2">
-      <!-- <img src="../../assets/platforms/netflix.png"> -->
+    <div class="two" v-if="noOfPlatforms === 2">
+      <img src="../../assets/platforms/netflix.png" />
+      <img src="../../assets/platforms/netflix.png" />
+    </div>
+
+    <div class="three" v-if="noOfPlatforms >= 3">
+      <img src="../../assets/platforms/netflix.png" />
+      <img src="../../assets/platforms/netflix.png" />
+      <img src="../../assets/platforms/netflix.png" />
+      <img src="../../assets/platforms/netflix.png" />
     </div>
   </div>
 </template>
@@ -29,16 +39,48 @@ export default {
 
   data() {
     return {
-      noOfPlatforms: 1,
+      noOfPlatforms: 2,
+      userPlatforms: null,
+      moviePlatforms: null,
+      finalPlatforms: null
+    };
+  },
+
+  computed: {
+    platCalc() {
+      let className = null;
+      if (this.noOfPlatforms === 1) {
+        className = "platform-container";
+      } else if (this.noOfPlatforms === 2) {
+        className = "two-plat";
+      } else {
+        className = "three-plus-plat";
+      }
+      return className;
+    },
+    changePlatName() {
+      let lowerCased = this.platforms.map((el) => {
+      return el.toLowerCase()
+      })
+      let replaceSpace = lowerCased.map((el) => {
+        return el.replace(/ /g, "_")
+      })
+      return replaceSpace;
+    }
+  },
+
+  methods: {
+    calcPlat() {
+      this.userPlatforms = this.changePlatName;
+      this.moviePlatforms = Object.keys(this.stream);
+      // this.noOfPlatforms = this.moviePlatforms.length;
     }
   },
 
   mounted() {
-    console.log(this.platforms, this.stream)
+    this.calcPlat();
   }
-
-  
-}
+};
 </script>
 
 <style scoped>
@@ -52,10 +94,55 @@ export default {
   border-radius: 50%;
 }
 
-img {
+.two-plat {
+  background-color: rgb(255, 255, 255);
+  width: 190px;
+  height: 90px;
+  border-radius: 50px;
+}
+
+.three-plus-plat {
+  background-color: rgb(255, 255, 255);
+  width: 250px;
+  height: 90px;
+  border-radius: 50px;
+}
+
+.one > img {
   padding: 5px;
   max-width: 100%;
   border-radius: 50%;
 }
 
+.two {
+  display: flex;
+  justify-content: space-evenly;
+  height: 100%;
+  align-items: center;
+}
+
+.two img {
+  padding: 2px;
+  max-width: 45%;
+  border-radius: 50%;
+}
+
+.three {
+  display: flex;
+  justify-content: flex-start;
+  height: 100%;
+  align-items: center;
+  border-radius: 50px;
+  overflow-x: scroll;
+}
+
+.three:first-child {
+  padding-left: 5px;
+}
+
+.three img {
+  padding: 2px;
+  max-width: 33%;
+  border-radius: 50%;
+}
 </style>
