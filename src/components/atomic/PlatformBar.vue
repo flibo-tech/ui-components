b856ee4c75652010b47be94128fc5a2a94931788
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-08-19 20:12:51 +0530

    max width height

--- src/components/atomic/PlatformBar.vue
+++ src/components/atomic/PlatformBar.vue
@@ -132,6 +132,8 @@ export default {
   width: 100%;
   height: 100%;
   border-radius: 50px;
+  max-width: 200px;
+  max-height: 80px;
   display: flex;
   align-items: center;
   justify-content: flex-start;
