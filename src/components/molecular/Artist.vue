f80c9a446dfc2bec2bb361fde40e5819341a3cdf
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-08-25 12:12:05 +0530

    added quickview

--- src/components/molecular/Artist.vue
+++ src/components/molecular/Artist.vue
@@ -1,9 +1,13 @@
 <template>
-  <Person :image="image" :width="width" :height="height" :name="name" />
+<div style="display: inline-block" @click="quickViewEnabled = true" >
+  <Person  :image="image" :width="width" :height="height" :name="name" />
+  <QuickView v-if="quickViewEnabled" :artistId="artistId" :skipContentId="skipContentId" />
+</div>
 </template>
 
 <script>
 import Person from "../atomic/Person";
+import QuickView from "../atomic/QuickView";
 export default {
   name: "Artist",
   // props: {
@@ -24,16 +28,19 @@ export default {
   //     required: true
   //   }
   // },
-  components: { Person },
+  components: { Person, QuickView },
   data() {
     return {
       // remove data when using props
       image: "https://image.tmdb.org/t/p/w500/qCpZn2e3dimwbryLnqxZuI88PTi.jpg",
       width: 200,
       height: 250,
-      name: "Some Name"
+      name: "Some Name",
+      artistId: 288143,
+      quickViewEnabled: false,
+      skipContentId: 122356
     };
-  }
+  },
 };
 </script>
 
